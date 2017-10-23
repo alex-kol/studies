5bf0be775545bab3b9c3f8b87326b0ba7f5d755f
alex-kol <32466732+alex-kol@users.noreply.github.com>
2017-10-23 21:00:06 +0300

    Update atm.cpp

--- lesson 3/atm.cpp
+++ lesson 3/atm.cpp
@@ -31,7 +31,7 @@ int main()
 	for (int i = START_ARRAY; i < LENGHT_ARRAY; i++){
 		pinArray[i] = 0;
 		clientData[i] = 0;
-    }
+    	}
 	pinArray[0] = ADMIN_PASS;
 
 	//writing pins to an array
@@ -46,100 +46,100 @@ int main()
 
 	for(;;){
 		for(int i = START_ARRAY; i < MAX_INPUT_PINS; i++){
-            printf("\nInput pin:\n");
-            scanf("%d",&pinCode);
-            if ((pinCode < MIN_PIN_CODE)||(pinCode > MAX_PIN_CODE)){
-                printf("\t---> Wrong pin! <---");
-                continue;
-            }
-            if (pinCode == ADMIN_PASS){
-                printf("\nHi, ADMIN\n");
-                printf("Your ~Balance~: %d\n\n",clientData[0]);
-                printf("1: Balance\n");
-                printf("2: Cash money\n");
-                printf("3: Refill\n");
-                printf("4: Zero data client\n");
-                printf("5: View all accounts\n");
-                printf("6: View all pins\n");
-                scanf("%d",&menuAtm);
-                break;
-            } else {
-                printf("1: Balance\n");
-                printf("2: Cash money\n");
-                printf("3: Refill\n");
-                scanf("%d",&menuAtm);
-                break;
-                }
-        }
+			printf("\nInput pin:\n");
+			scanf("%d",&pinCode);
+			if ((pinCode < MIN_PIN_CODE)||(pinCode > MAX_PIN_CODE)){
+				printf("\t---> Wrong pin! <---");
+				continue;
+			}
+			if (pinCode == ADMIN_PASS){
+				printf("\nHi, ADMIN\n");
+				printf("Your ~Balance~: %d\n\n",clientData[0]);
+				printf("1: Balance\n");
+				printf("2: Cash money\n");
+				printf("3: Refill\n");
+				printf("4: Zero data client\n");
+				printf("5: View all accounts\n");
+				printf("6: View all pins\n");
+				scanf("%d",&menuAtm);
+				break;
+			} else {
+				printf("1: Balance\n");
+				printf("2: Cash money\n");
+				printf("3: Refill\n");
+				scanf("%d",&menuAtm);
+				break;
+				}
+		}
 		switch(menuAtm){
 			case MENU_ATM_ONE:	for (int i = START_ARRAY; i < LENGHT_ARRAY; i++){
-									if (pinCode == pinArray[i]){
-										printf("\nBalans: %d\n",clientData[i]);
-									}
-								}
-								break;
+							if (pinCode == pinArray[i]){
+								printf("\nBalans: %d\n",clientData[i]);
+							}
+						}
+						break;
 			case MENU_ATM_TWO:	printf("\nHow much to withdraw money?\n");
-								scanf("%d",&inputCash);
-								for (int i = START_ARRAY; i < LENGHT_ARRAY; i++){
-			                        if (clientData[i] > PLUS_BALANCE){
-			                            if (pinCode == pinArray[i]){
-			                                clientData[i] = clientData[i] - inputCash;
-									    }
-			                        }else {
-			                            printf("\t-->No money on the account!!!<--");
-			                            break;
-			                            }
-			                    }
-							   	break;
-			case MENU_ATM_THREE:printf("\nEnter the amount of the replenishment:\n");
-								for (int i = START_ARRAY; i < LENGHT_ARRAY; i++){
-									if (pinCode == pinArray[i]){
-				                           printf("Your balans: %d\n",clientData[i]);
-									}
-								}
-								scanf("%d",&refillCash);
-								for (int i = START_ARRAY; i < LENGHT_ARRAY; i++){
-									if (pinCode == pinArray[i]){
-				                           clientData[i] = clientData[i] + refillCash;
-									}
-								}
-								break;
+						scanf("%d",&inputCash);
+						for (int i = START_ARRAY; i < LENGHT_ARRAY; i++){
+							if (clientData[i] > PLUS_BALANCE){
+							    if (pinCode == pinArray[i]){
+								clientData[i] = clientData[i] - inputCash;
+							    }
+							}else {
+							    printf("\t-->No money on the account!!!<--");
+							    break;
+							    }
+						}
+						break;
+			case MENU_ATM_THREE:	printf("\nEnter the amount of the replenishment:\n");
+						for (int i = START_ARRAY; i < LENGHT_ARRAY; i++){
+							if (pinCode == pinArray[i]){
+								printf("Your balans: %d\n",clientData[i]);
+							}
+						}
+						scanf("%d",&refillCash);
+						for (int i = START_ARRAY; i < LENGHT_ARRAY; i++){
+							if (pinCode == pinArray[i]){
+								clientData[i] = clientData[i] + refillCash;
+							}
+						}
+						break;
 			case MENU_ATM_FOUR:	printf("\n***Do I clear all accounts??? Are you sure???***\n");
-								printf("\t\t***Y or N***\n");
-								getchar();
-								scanf(" %c",&clearData);
-								if ((clearData == 'Y') || (clearData == 'y')){
-									for (int i = START_ARRAY + 1; i < LENGHT_ARRAY; i++){
-										clientData[i] = 0;
-									}
-									printf("Balance ADMIN: %d\n",clientData[0]);
-									for(int i = START_ARRAY + 1; i < LENGHT_ARRAY; i++){
-			                            printf("%d|",clientData[i]);
-			                        }
+						printf("\t\t***Y or N***\n");
+						getchar();
+						scanf(" %c",&clearData);
+						if ((clearData == 'Y') || (clearData == 'y')){
+							for (int i = START_ARRAY + 1; i < LENGHT_ARRAY; i++){
+								clientData[i] = 0;
+							}
+							printf("Balance ADMIN: %d\n",clientData[0]);
+							for(int i = START_ARRAY + 1; i < LENGHT_ARRAY; i++){
+							    printf("%d|",clientData[i]);
+							}
+						}
+						break;
+			case MENU_ATM_FIVE:	printf("\n***View ALL accounts (y/n)???***\n");
+						getchar();
+						scanf(" %c",&clearData);
+						if ((clearData == 'Y') || (clearData == 'y')){
+							printf("Balance ADMIN: %d\nBalance clients: ",clientData[0]);
+								for (int i = START_ARRAY + 1; i < LENGHT_ARRAY; i++){
+									printf("%d|",clientData[i]);
 								}
-								break;
-            case MENU_ATM_FIVE:	printf("\n***View ALL accounts (y/n)???***\n");
-								getchar();
-								scanf(" %c",&clearData);
-								if ((clearData == 'Y') || (clearData == 'y')){
-			                        printf("Balance ADMIN: %d\nBalance clients: ",clientData[0]);
-									for (int i = START_ARRAY + 1; i < LENGHT_ARRAY; i++){
-										printf("%d|",clientData[i]);
-									}
-								}
-								break;
-            case MENU_ATM_SIX:	printf("\n***View ALL pins (y/n)???***\n");
-								getchar();
-								scanf(" %c",&clearData);
-								if ((clearData == 'Y') || (clearData == 'y')){
-			                        printf("Pin ADMIN: %d\nPins clients: ",pinArray[0]);
-									for (int i = START_ARRAY + 1; i < LENGHT_ARRAY; i++){
-										printf("%d|",pinArray[i]);
-									}
-								}
-								break;
-			default: 			printf("\n\t--->Wrong choose!<---\n");
-                    			break;
+						}
+						break;
+			case MENU_ATM_SIX:	printf("\n***View ALL pins (y/n)???***\n");
+						getchar();
+						scanf(" %c",&clearData);
+						if ((clearData == 'Y') || (clearData == 'y')){
+							printf("Pin ADMIN: %d\nPins clients: ",pinArray[0]);
+							for (int i = START_ARRAY + 1; i < LENGHT_ARRAY; i++){
+								printf("%d|",pinArray[i]);
+							}
+						}
+						break;
+			default: 		printf("\n\t--->Wrong choose!<---\n");
+						break;
 		}
 	}
 	return 0;
